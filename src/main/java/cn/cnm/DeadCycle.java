17d0143455f1c95135c6c9614d888cd19167ad70
ziyaliuye <171242039@qq.com>
2019-09-28 20:45:08 +0800

    first commit.....

---
+++ src/main/java/cn/cnm/DeadCycle.java
@@ -0,0 +1,19 @@
+package cn.cnm;
+
+public class DeadCycle {
+    public static void main(String[] args) {
+        // 死循环
+//        for (;;){
+//
+//        }
+        // 死循环
+//        while (true) {
+//
+//        }
+
+        do{
+
+        }while (true);
+
+    }
+}
