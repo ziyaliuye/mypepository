bc0ad0f09819db6c0e590c772505d8c992675d18
ziyaliuye <171242039@qq.com>
2019-09-28 23:15:00 +0800

    Add several examples....

--- src/main/java/cn/cnm/Test.java
+++ src/main/java/cn/cnm/Test.java
@@ -2,6 +2,12 @@ package cn.cnm;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println(Math.sqrt(11));
+        for1:
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                System.out.println("-----");
+                break for1;
+            }
+        }
     }
 }
