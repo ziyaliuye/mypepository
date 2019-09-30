755b51f27060ba6c9aa5f16c575e944450bd8b69
ziyaliuye <171242039@qq.com>
2019-10-01 01:44:27 +0800

    perfect code....

--- src/main/java/cn/cnm/ArrayDemo.java
+++ src/main/java/cn/cnm/ArrayDemo.java
@@ -99,6 +99,8 @@ public class ArrayDemo {
         // 完整的数组复制
         int[] array3 = new int[]{1, 2, 3, 4, 5};
         int[] array4 = new int[5];
+        int[] array7 = new int[5];
+        int[] array8 = new int[5];
         for (int x = 0; x < array3.length; x++) {
             array4[x] = array3[x];
         }
@@ -110,6 +112,23 @@ public class ArrayDemo {
         for (int i : array4) {
             System.out.println("array4：" + i);
         }
+
+        // 其他复制方法
+        System.arraycopy(array3, 0, array7, 0, array3.length);
+        array8 = array3.clone();
+        for (int i : array7) {
+            System.out.println("array7：" + i);
+        }
+        for (int i : array8) {
+            System.out.println("array8：" + i);
+        }
+        // 修改值， 看是否是完整克隆
+        array7[0] = 100;
+        array8[1] = 100;
+        for (int i : array3) {
+            System.out.println("array3：" + i);
+        }
+
 
         // 数组反转， 将数组的数据顺序反转
         int[] array5 = new int[]{1, 2, 3, 4, 5, 6};
@@ -152,6 +171,74 @@ public class ArrayDemo {
                 // 找到后当然就退出循环
                 break;
             }
+
+            // 数组排序
+            int[] array9 = new int[]{3, 1, 5, 2, 4, 9, 8, 6, 7};
+            // 冒泡排序
+            for (int i = 0; i < array9.length - 1; i++) {
+                for (int j = 0; j < array9.length - 1 - i; j++) {
+                    // 拿每一个元素进行比较， 将最小的值塞左边
+                    if (array9[j] > array9[j + 1]) {
+                        int tmp = array9[j];
+                        array9[j] = array9[j + 1];
+                        array9[j + 1] = tmp;
+                    }
+                }
+            }
+
+            for (int i : array9) {
+                System.out.println("冒泡排序后array9：" + i);
+            }
+        }
+
+        // 快速排序
+        int[] array10 = new int[]{3, 1, 5, 2, 4, 9, 8, 6, 7};
+        ArrayDemo.quickSort(array10);
+        for (int i : array10) {
+            System.out.println("快速排序后array10：" + i);
+        }
+    }
+
+    // 快速排序
+    private static void quickSort(int[] data) {
+        subSort(data, 0, data.length - 1);
+    }
+
+    // 交换方法， 将传入的数据2个索引对应的数据进行互换
+    private static void swap(int[] data, int i, int j) {
+        int temp = data[i];
+        data[i] = data[j];
+        data[j] = temp;
+    }
+
+    // 快速排序逻辑, 参数一：要排序的数组  参数二：一般是0  参数三：第一次是数组长度
+    private static void subSort(int[] data, int start, int end) {
+        if (start < end) {
+            int pivot = data[start];
+            int low = start + 1;
+            // 最大索引， 为了方便后续流程
+            int high = end + 1;
+            while (true) {
+                // low指针开始往右走， 遇到比Pivot大的就跳出循环， 否则一直往右走
+                while (low < end && data[++low] - pivot <= 0) ;
+                // low指针开始往右走， 遇到比Pivot大的就跳出循环， 否则一直往右走
+                while (high > start && data[--high] - pivot >= 0) ;
+                // 指针都已经停止， 需要将对应的数据进行交换
+                if (low < high) {
+                    swap(data, low, high);
+                } else {
+                    // 一旦走到这说 high 指针比 low指针小了， 跳出循环准备Pivot和high数据交换
+                    break;
+                }
+            }
+            // 这轮排序结束，Pivot(对应索引是start)和high数据交换
+            swap(data, start, high);
+
+            // 递归调用， 以high处的指针为中心点， 循环排序左右两边的数据
+            // 左边的数据
+            subSort(data, start, high - 1);
+            // 右边的数据
+            subSort(data, high + 1, end);
         }
     }
 }
