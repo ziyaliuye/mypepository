59a0658cd767c2c750628f3c00559b21e1f87f3b
ziyaliuye <51473277+ziyaliuye@users.noreply.github.com>
2019-10-11 15:26:21 +0800

    Perfect code....

---
+++ src/main/java/main/cn/cnm/TreeSetDemo.java
@@ -0,0 +1,24 @@
+package main.cn.cnm;
+
+import java.util.TreeSet;
+
+/**
+ * @author lele
+ * @version 1.0
+ * @Description 可以按照添加的元素指定属性进行排序， 放入 TreeSet 集合的对象必须是同一个类 new 出来的
+ * TreeSet底层采用红黑树的存储结构
+ * @Email 414955507@qq.com
+ * @date 2019/10/11 15:08
+ */
+public class TreeSetDemo {
+    public static void main(String[] args) {
+        TreeSet treeSet = new TreeSet();
+        treeSet.add(1);
+        treeSet.add(3);
+        treeSet.add(2);
+        // treeSet.add(new Person());
+        /* 涉及对象的排序， 对象需要重写compareTo方法（自然排序） 或者 使用Collections.sort()时传入Comparator对象（定制排序） */
+        // 对象没有重写compareTo()方法则会报错
+        System.out.println(treeSet);
+    }
+}
